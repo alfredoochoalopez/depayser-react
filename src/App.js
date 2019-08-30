e9ecb4f06c3da428d5070b77456467eeabab58a3
Alfredo Ochoa <alfredo.ochoa@walmart.com>
2019-08-29 21:44:44 -0500

    Added Routing and Axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import axios from "axios";
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import './App.scss';
+import Details from "./Details";
 import Genders from "./Genders";
 import Home from "./Home";
+export const http = axios.create({
+  baseURL: 'https://mfwkweb-api.clarovideo.net'
+})
 
 const App = () => (
   <div>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/genero/:id" component={Genders} />
-      {/* <Route path="/about" component={About} /> */}
+      <Route path="/detalles/:id" component={Details} />
+      <Route component={Home} />
+
     </Switch>
   </div>
 );
